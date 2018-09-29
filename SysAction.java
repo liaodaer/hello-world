06be2a2649643636763dc80c28ac21fc958f5674
liaodaer <245870595@qq.com>
2018-09-29 10:52:51 +0800

    Add files via upload

---
+++ SysAction.java
@@ -0,0 +1,25 @@
+package com.model;
+
+/**
+ * Created by Administrator on 2018/9/7.
+ */
+public class SysAction {
+    private String actionid;
+    private String actionname;
+
+    public String getActionid() {
+        return actionid;
+    }
+
+    public void setActionid(String actionid) {
+        this.actionid = actionid;
+    }
+
+    public String getActionname() {
+        return actionname;
+    }
+
+    public void setActionname(String actionname) {
+        this.actionname = actionname;
+    }
+}
